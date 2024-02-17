a77e1a061a3b66c4ba6a01609a3606fb472c5863
JackTheRipper2187 <ulilf442@gmail.com>
2024-02-17 22:22:53 +0700

    merapikan kode

--- Praktikum_Fungsi.java
+++ Praktikum_Fungsi.java
@@ -2,7 +2,7 @@ import java.util.Arrays;
 public class Praktikum_Fungsi {
 
     public static void main(String[] args) {
-        // Data input
+    
         int[][] stockData = {
                 {10, 5, 15, 7},
                 {6, 11, 9, 12},
@@ -12,16 +12,16 @@ public class Praktikum_Fungsi {
 
         int[] hargaBunga = {75000, 50000, 60000, 10000};
 
-        // 1. Display income for each branch if all flowers are sold out
+    
         PendapatanSetiapCabang(stockData, hargaBunga);
 
-        // 2. Get the stock of each flower in RoyalGarden 4 after considering additional information
+      
         int[] InfoTambahan = {-1, -2, 0, -5};
         int[] stokRoyalGarden4SetelahPengurangan = mendapatkanStokRoyalGarden4(stockData[3], InfoTambahan);
         System.out.println("Stok RoyalGarden 4 setelah pengurangan: " + Arrays.toString(stokRoyalGarden4SetelahPengurangan));
     }
 
-    // 1. Function to display income for each branch if all flowers are sold out
+
     private static void PendapatanSetiapCabang(int[][] stockData, int[] hargaBunga) {
         int totalPendapatan = 0;
 
@@ -42,7 +42,7 @@ public class Praktikum_Fungsi {
         System.out.println("Total pendapatan dari semua cabang: " + totalPendapatan);
     }
 
-    // 2. Function to get the stock of each flower in RoyalGarden 4 after considering additional information
+
     private static int[] mendapatkanStokRoyalGarden4(int[] stokRoyalGarden4, int[] InfoTambahan) {
         int[] StokTerupdate = Arrays.copyOf(stokRoyalGarden4, stokRoyalGarden4.length);
 
