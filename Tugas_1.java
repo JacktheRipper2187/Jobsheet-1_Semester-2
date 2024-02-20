9e0c8e58cd5f29c54608a8f532391d6b04110722
JackTheRipper2187 <ulilf442@gmail.com>
2024-02-20 19:42:18 +0700

    char

--- Tugas_1.java
+++ Tugas_1.java
@@ -2,27 +2,41 @@ import java.util.Scanner;
 
 public class Tugas_1 {
     public static void main(String[] args) {
-        // Inisialisasi array KODE dan KOTA
-        String[] KODE = {"A", "B", "D", "E", "F", "G", "H", "L", "N", "T"};
-        String[] KOTA = {"BANTEN", "JAKARTA", "BANDUNG", "CIREBON", "BOGOR", "PEKALONGAN", "SEMARANG", "SURABAYA", "MALANG", "TEGAL"};
+      
+        char[] KODE = {'A', 'B', 'D', 'E', 'F', 'G', 'H', 'L', 'N', 'T'};
+        char[][] KOTA = {
+            {'B', 'A', 'N', 'T', 'E', 'N'},
+            {'J', 'A', 'K', 'A', 'R', 'T', 'A'},
+            {'B', 'A', 'N', 'D', 'U', 'N', 'G'},
+            {'C', 'I', 'R', 'E', 'B', 'O', 'N'},
+            {'B', 'O', 'G', 'O', 'R'},
+            {'P', 'E', 'K', 'A', 'L', 'O', 'N', 'G', 'A', 'N'},
+            {'S', 'E', 'M', 'A', 'R', 'A', 'N', 'G'},
+            {'S', 'U', 'R', 'A', 'B', 'A', 'Y', 'A'},
+            {'M', 'A', 'L', 'A', 'N', 'G'},
+            {'T', 'E', 'G', 'A', 'L'}
+        };
 
-        // Input kode plat nomor dari pengguna
+       
         Scanner sc = new Scanner(System.in);
         System.out.print("Masukkan kode plat nomor (satu huruf): ");
-        String inputKode = sc.next();
+        char inputKode = sc.next().toUpperCase().charAt(0); // jika pengguna menginput huruf kecil akan otomatis terbaca "uppercase"
 
         // Cari nama kota berdasarkan kode plat nomor
         int index = -1;
         for (int i = 0; i < KODE.length; i++) {
-            if (KODE[i].equals(inputKode)) {
+            if (KODE[i] == inputKode) {
                 index = i;
                 break;
             }
         }
 
-        // Menampilkan hasil
         if (index != -1) {
-            System.out.println("Nama kota untuk kode plat " + inputKode + " adalah: " + KOTA[index]);
+            System.out.print("Nama kota untuk kode plat " + inputKode + " adalah: ");
+            for (int j = 0; j < KOTA[index].length; j++) {
+                System.out.print(KOTA[index][j]);
+            }
+            System.out.println();
         } else {
             System.out.println("Kode plat nomor tidak ditemukan.");
         }
